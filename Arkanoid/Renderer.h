c14fc7ac77a1a20a34a0bcd0f98557912e37043b
Alex Espada <alex.espada@enti.cat>
2018-12-18 13:11:08 +0100

    New types added: Color, Text, Font. Rendered modified to accept mtdl types

--- Arkanoid/Renderer.h
+++ Arkanoid/Renderer.h
@@ -1,8 +1,42 @@
 #pragma once
-class Renderer
-{
-public:
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <string.h>
+#include <unordered_map>
+#include "../dep/inc/mtdl/mtdl.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
+
+class Renderer {
+
+private:
+	SDL_Renderer *m_renderer = nullptr;
+	SDL_Window *m_window = nullptr;
+	std::unordered_map<std::string, SDL_Texture*> m_textureData;
+	std::unordered_map<std::string, TTF_Font*> m_fontData;
+	static Renderer *renderer;
 	Renderer();
+
+public:
+	static Renderer *Instance()
+	{
+		if (renderer == nullptr) { renderer = new Renderer; }
+		return renderer;
+	};
+
 	~Renderer();
+	 void Clear();
+	 void Render();
+	 void LoadFont(mtdl::Font font);
+	 void LoadTexture(const std::string &id, const std::string &path);
+	 void LoadTextureText(const std::string &fontId, mtdl::Text text);
+	 mtdl::Vector2 GetTextureSize(const std::string &id);
+	 void PushImage(const std::string &id,const SDL_Rect &rect);
+	 void PushSprite(const std::string &id,const SDL_Rect &rectSprite,const SDL_Rect &rectPos);
+	 void PushRotatedSprite(const std::string &id, const SDL_Rect &rectSprite, const SDL_Rect &rectPos, float angle);
+	 void SetRendreDrawColor(int r, int g, int b);	 
 };
+
 
--- dep/inc/mtdl/mtdl.h
+++ dep/inc/mtdl/mtdl.h
@@ -1,27 +1,54 @@
 #pragma once
+#include <string>
 
 namespace mtdl {
 
 	/*---TYPES---*/
-	
+	#pragma region Types
+
 	struct Vector2 {		//Vector 2D
 		int x;
 		int y;
 
 		Vector2(int _x, int _y) : x(_x), y(_y) {}
-	}; 
+	};
 
 	struct Rect {			//Rectangle
 		Vector2 position;
 		int w;
 		int h;
 
-		Rect(int _x, int _y, int _w, int _h) : position.x(_x), position.y(_y), w(_w), h(_h){}
+		Rect(int _x, int _y, int _w, int _h) : position.x(_x), position.y(_y), w(_w), h(_h) {}
 	};
 
+	struct Color		//Color
+	{
+		int r, g, b, a;
+
+		Color(int _r, int _g, int _b, int _a) : r(_r), g(_g), b(_b), a(_a) {}
+	};
+
+	struct Font		//Font
+	{
+		std::string path;
+		int size;
+		std::string id;
+
+		Font(std::string _path, int _size, std::string _id) : path(_path), size(_size), id(_id) {}
+	};
+
+	struct Text		//Text
+	{
+		std::string text;
+		Color color;
+		std::string id;
+
+		Text(std::string _text, Color _color, int _id) : text(_text), color(_color), id(_id) {}
+	};
+	#pragma endregion
 
 	/*---FUNCTIONS---*/
-
+	#pragma region Functions
 	bool VectorRectCollision(Vector2 v, Rect r) {
 		return (v.x >= r.position.x) && (v.x <= (r.position.x + r.w)) && (v.y >= r.position.y) && (v.y <= (r.position.y + r.h));
 	}
@@ -29,4 +56,6 @@ namespace mtdl {
 	bool RectRectCollision(Rect r, Rect t) {
 		return VectorRectCollision(r.position, t) || VectorRectCollision(new Vector2(r.position.x + r.w, r.position.y), t) || VectorRectCollision(new Vector2(r.position.x, r.position.y + r.h), t) || VectorRectCollision(new Vector2(r.position.x + r.w, r.position.y + r.h) + r.w, t);
 	}
+	#pragma endregion
+
 }
